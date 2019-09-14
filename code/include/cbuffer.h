2d264b22d6f990b422b9d78c4c34e0eb29a90a63
balazs <marjanbalazs@outlook.com>
2019-09-14 22:28:25 +0200

    Initial commit of the files

---
+++ code/include/cbuffer.h
@@ -0,0 +1,27 @@
+#ifndef CBUFFER_H
+#define CBUFFER_H_
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    void* element_cbuffer;
+    size_t sizeof_element;
+    size_t capacity;
+    size_t read;
+    size_t write;
+    size_t full;
+} cbuffer_t;
+
+typedef cbuffer_t* cbuffer_handle_t;
+
+//------------------Function prototypes------------------
+
+cbuffer_handle_t construct_cbuffer(size_t capacity, size_t elem_size);
+void destroy_cbuffer(cbuffer_handle_t cbuffer);
+int cbuffer_isfull(cbuffer_handle_t cbuffer);
+int cbuffer_isempty(cbuffer_handle_t cbuffer);
+int cbuffer_put(void* source_element, cbuffer_handle_t cbuffer);
+void* cbuffer_get(cbuffer_handle_t cbuffer);
+
+#endif
---
+++ code/src/cbuffer.c
@@ -0,0 +1,67 @@
+#include "..\include\cbuffer.h"
+
+cbuffer_handle_t construct_cbuffer(size_t capacity, size_t elem_size)
+{
+    cbuffer_handle_t cbuffer = malloc( sizeof (cbuffer_t));
+    cbuffer->element_cbuffer = malloc( elem_size * capacity);
+    cbuffer->sizeof_element = elem_size;
+    cbuffer->capacity = capacity;
+    cbuffer->read = 0;
+    cbuffer->write = 0;
+    cbuffer->full = 0;
+}
+
+void destroy_cbuffer(cbuffer_handle_t cbuffer)
+{
+    free(cbuffer->element_cbuffer);
+    free(cbuffer);
+}
+
+int cbuffer_put(void* source_element, cbuffer_handle_t cbuffer)
+{
+    int ret;
+    if ( cbuffer_isfull(cbuffer) )
+    {
+        ret = 0;
+    }
+    else
+    {
+        void* ptr =  cbuffer->element_cbuffer +  cbuffer->write * cbuffer->sizeof_element;
+        memcpy(ptr, source_element, cbuffer->sizeof_element);
+        cbuffer->write++;
+        if ( cbuffer->write % cbuffer->capacity == cbuffer->read % cbuffer->capacity )
+        {
+            cbuffer->full = 1;
+        }
+        ret = 1;
+    }
+    return ret;
+}
+
+void* cbuffer_get( cbuffer_handle_t cbuffer)
+{
+    void* retptr;
+    
+    if( cbuffer_isempty(cbuffer) != 0)
+    {
+        retptr = NULL;
+    }
+    else
+    {
+        retptr = cbuffer->element_cbuffer + cbuffer->read * cbuffer->sizeof_element;
+        cbuffer->full = 0;
+        cbuffer->read++;
+    }
+
+    return retptr;
+}
+
+int cbuffer_isfull(cbuffer_handle_t cbuffer)
+{
+    return cbuffer->full;
+}
+
+int cbuffer_isempty(cbuffer_handle_t cbuffer)
+{
+    return ( cbuffer->read == cbuffer->write ) && (cbuffer->full == 0);
+}
