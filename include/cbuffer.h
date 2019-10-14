2c44210708b716d9c6c1f89a27c2e31c172411c5
balazs <balazs@balazs-pc>
2019-10-14 22:01:41 +0200

    Minor fixes to style and pointers

--- include/cbuffer.h
+++ include/cbuffer.h
@@ -7,23 +7,23 @@
 /*------------------Types--------------------------------*/
 
 typedef struct {
-    size_t sizeof_element;
-    size_t capacity;
-    size_t read;
-    size_t write;
-    size_t full;
-    void* element_cbuffer;
+	size_t sizeof_element;
+	size_t capacity;
+	size_t read;
+	size_t write;
+	size_t full;
+	void *element_cbuffer;
 } cbuffer_t;
 
-typedef cbuffer_t* cbuffer_handle_t;
+typedef cbuffer_t *cbuffer_handle_t;
 
 /*------------------Function prototypes------------------*/
 
-cbuffer_handle_t construct_cbuffer(size_t capacity, size_t elem_size);
-void destroy_cbuffer(cbuffer_handle_t cbuffer);
-int cbuffer_isfull(cbuffer_handle_t cbuffer);
-int cbuffer_isempty(cbuffer_handle_t cbuffer);
-int cbuffer_put(void* source_element, cbuffer_handle_t cbuffer);
-void* cbuffer_get(cbuffer_handle_t cbuffer);
+cbuffer_t* construct_cbuffer(size_t capacity, size_t elem_size);
+void destroy_cbuffer(cbuffer_t* cbuffer);
+int cbuffer_isfull(cbuffer_t* cbuffer);
+int cbuffer_isempty(cbuffer_t* cbuffer);
+int cbuffer_put(void *source_element, cbuffer_t* cbuffer);
+void* cbuffer_get(cbuffer_t* cbuffer);
 
 #endif
--- main/main.c
+++ main/main.c
@@ -4,25 +4,25 @@
 int main(void)
 {
 	printf("Hello from main!\n");
-    cbuffer_t* cb = construct_cbuffer(10, sizeof(int));
-    int x = 5;
-    int y = 6;
-    int z = 7;
-    cbuffer_put(&x, cb);
-    cbuffer_put(&y, cb);
-    cbuffer_put(&z, cb);
+	cbuffer_t *cb = construct_cbuffer(10, sizeof(int));
+	int x = 5;
+	int y = 6;
+	int z = 7;
+	cbuffer_put(&x, cb);
+	cbuffer_put(&y, cb);
+	cbuffer_put(&z, cb);
 
-    if( cbuffer_isempty(cb) ) {
-    	printf("The buffer is not supposed to be empty!\n");
-    } else {
-    	printf("All is good!\n");
-    }
-    int * p = cbuffer_get(cb);
-    printf("%d\n", *p);
-    p = cbuffer_get(cb);
-    printf("%d\n", *p);
-    p = cbuffer_get(cb);
-    printf("%d\n", *p);
+	if (cbuffer_isempty(cb)) {
+		printf("The buffer is not supposed to be empty!\n");
+	} else {
+		printf("All is good!\n");
+	}
+	int *p = cbuffer_get(cb);
+	printf("%d\n", *p);
+	p = cbuffer_get(cb);
+	printf("%d\n", *p);
+	p = cbuffer_get(cb);
+	printf("%d\n", *p);
 
-    destroy_cbuffer(cb);
+	destroy_cbuffer(cb);
 }
